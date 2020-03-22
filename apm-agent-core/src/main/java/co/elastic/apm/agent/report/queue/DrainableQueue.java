18d66cf84a6e734d57f9e56f426aae45b12603cc
Felix Barnsteiner <felix.barnsteiner@elastic.co>
2020-03-22 13:28:48 +0100

    Remove unused classes

--- apm-agent-core/src/main/java/co/elastic/apm/agent/report/queue/DrainableQueue.java
+++
@@ -1,9 +0,0 @@
-package co.elastic.apm.agent.report.queue;
-
-import org.jctools.queues.MessagePassingQueue;
-
-public interface DrainableQueue<E> {
-    boolean offer(E element);
-
-    void drain(MessagePassingQueue.Consumer<E> consumer);
-}
